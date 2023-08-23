537355e5a201453a5291c9665f713e5fb251cf26
sarvajeetnarkhede <sarvajeetnarkhede5072@gmail.com>
2023-08-23 19:18:41 +0530

    Hi

---
+++ ePizzaHub/ePizzaHub.WebUI/Configuration/ConfigureDependencies.cs
@@ -0,0 +1,23 @@
+﻿using ePizzaHub.Services.Implementations;
+using ePizzaHub.Services.Interfaces;
+using ePizzaHub.WebUI.Helpers;
+using ePizzaHub.WebUI.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ePizzaHub.WebUI.Configuration
+{
+	public class ConfigureDependencies
+    {
+        public static void AddServices(IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            services.AddTransient<IUserAccessor, UserAccessor>();
+            services.AddTransient<IPaymentService, PaymentService>();
+            services.AddTransient<ICatalogService, CatalogService>();
+            services.AddTransient<ICartService, CartService>();
+            services.AddTransient<IOrderService, OrderService>();
+            services.AddTransient<IFileHelper, FileHelper>();
+        }
+    }
+}
